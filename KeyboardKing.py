2dad69d9ec4a6c6f377272818741aff6cfb6b131
KubSka <skalkaj@jirovcovka.net>
2024-10-30 19:44:42 +0100

    Refactor create_menu and create_canvas methods

--- KeyboardKing.py
+++ KeyboardKing.py
@@ -14,6 +14,28 @@ class KeyboardKing(tk.Tk):
         self.title("Keyboard King")
         self.geometry("800x600")
 
+
+
+        self.create_menu()
+        self.create_canvas()
+        #self.show_rules()
+
+    def create_menu(self):
+        menu = tk.Menu(self)
+        self.config(menu=menu)
+        game_menu = tk.Menu(menu, tearoff=0)
+        menu.add_cascade(label="Game", menu=game_menu)
+        game_menu.add_command(label="Start", command=self.start_game) #napsat start_game
+        game_menu.add_command(label="End", command=self.quit)
+
+        #zobrazení pravidel?
+
+    def create_canvas(self):
+        self.canvas = rk.canvas(self, width=800, height=500, bg="white")
+
+    #def start_game(self):
+        
+
 if __name__ == "__main__":
     app = KeyboardKing()
     app.mainloop()
